ba35abc24a8c5ed4d02db8a7fc25f5ef57012a48
Daniel Juarez <juarezz.daniel97@gmail.com>
2025-02-24 17:26:50 -0300

    Refactory code 2.2

--- src/components/shared/CounterTimer.jsx
+++ src/components/shared/CounterTimer.jsx
@@ -4,14 +4,14 @@ const Countdown = () => {
     const calculateTimeLeft = () => {
         const eventDate = new Date("2025-02-19");
         const difference = eventDate - new Date();
-        let timeLeft = {};
+        let timeLeft = {Días: 0, Horas: 0, Minutos: 0};
 
         if (difference > 0) {
-        timeLeft = {
-            Días: Math.floor(difference / (1000 * 60 * 60 * 24)),
-            Horas: Math.floor((difference / (1000 * 60 * 60)) % 24),
-            Minutos: Math.floor((difference / 1000 / 60) % 60),
-        };
+            timeLeft = {
+                Días: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                Horas: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                Minutos: Math.floor((difference / 1000 / 60) % 60),
+            };
         }
         return timeLeft;
     };
@@ -29,10 +29,14 @@ const Countdown = () => {
         <div className="p-4 flex justify-center space-x-4 font-bold text-lg sm:text-xl">
             {Object.entries(timeLeft).map(([unit, value]) => (
                 <div key={unit} className="flex flex-col items-center">
-                <span className="bg-terracota text-white px-3 py-2 rounded-md text-lg sm:text-xl">
-                    {value}
-                </span>
-                <span className="text-sm text-crema">{unit} </span>
+                    
+                    <span className="bg-terracota text-white px-3 py-2 rounded-md text-lg sm:text-xl">
+                        {value}
+                    </span>
+
+                    <span className="text-sm text-crema">
+                        {unit} 
+                    </span>
                 </div>
             ))}
         </div>
